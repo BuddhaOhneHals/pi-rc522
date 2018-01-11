0dfa5b5c82bf168388a1f34b8019aaf8da550bb1
dreilly369 <executivewd@hotmail.com>
2018-01-10 19:02:25 -0800

    Create rfcrypto.py

---
+++ pirc522/rfcrypto.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import os
+import hashlib
+from base64 import b64encode, b64decode
+from Crypto.Cipher import AES
+
+#
+# Add AES-128 CBC encryption functions for use with the
+# RC-522 Library's read_card and write_card
+# The MAX_PT_LEN takes into account th
+# 543 bytes = 748 bytes after encryption (4bytes left over)
+# 544 bytes = 768 bytes after encryption (too big)
+
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s: s[0:-ord(s[-1])]
+SECURE_ITR = 400000  # Number of interations. More secure (slower bruting)
+BASIC_ITR = 250000  # Number of interations. Less secure (faster bruting)
+SALT_LEN = 32  # 32 byte (256 bit) random salt length for key generation
+MAX_PT_LEN = 543  # (15(3*16)+2(3*16)) = 752
+
+class Cipher:
+
+    padded = ""
+
+    def __init__(self, password, ic=4e5, salt=None):
+        if salt is None:
+            self.salt = os.urandom(SALT_LEN)
+        else:
+            self.salt = salt
+        self.key, self.salt = self.gen_key(password, self.salt, ic)
+
+    def gen_key(self, pw, iteration_count=4e5, salt=None):
+        if salt is None:
+            salt = self.salt
+        if iteration_count is None:
+            iteration_count = 4e5
+            print iteration_count
+        os.GRND_RANDOM = True  # Switch to using /dev/random
+        k = hashlib.pbkdf2_hmac('sha256',
+                                b'%s' % pw,
+                                b'%s' % salt,
+                                400000)
+        return (k, self.salt)
+
+    def encrypt(self, plaintext):
+        plaintext = pad(plaintext)
+        self.padded = plaintext
+        os.GRND_RANDOM = True  # Switch to using /dev/random
+        iv = os.urandom(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return b64encode(iv + cipher.encrypt(plaintext))
+
+    def decrypt(self, enc):
+        enc = b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return unpad(cipher.decrypt(enc[16:]))
+
+    def hmac(self, data_str, key=None):
+        if key is None:
+            key = b64encode(os.urandom(SALT_LEN))
+        exp_key, exp_salt = self.gen_key(key, SECURE_ITR)
+        macer = hashlib.sha256()
+        macer.update(data_str + exp_key)
+        khmac = macer.digest()
+        return (key, khmac)
+
+    def random(self, num_bytes):
+        print "getting %d bytes" % num_bytes
+        os.GRND_RANDOM = True  # Switch to using /dev/random
+        block = os.urandom(num_bytes)
+        return block
